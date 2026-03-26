43b1d43721fdc3ea19769f2b6d7fdcdefced388c
ShreyTriesToCode <111811929+ShreyTriesToCode@users.noreply.github.com>
2026-03-26 14:16:11 +0000

    Solve: #24 Binary Search [Mar 26]

---
+++ solutions/2026/03/prob_0024_binary_search_26mar.py
@@ -0,0 +1,25 @@
+from typing import List, Optional
+
+class Solution:
+    def binarySearch(self, nums: List[int], target: int) -> Optional[int]:
+        """
+        Approach: This problem can be solved using a modified binary search algorithm.
+        Time Complexity: O(log n)
+        Space Complexity: O(1)
+        """
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return None
+
+# --- Test Cases ---
+if __name__ == '__main__':
+    s = Solution()
+    print(s.binarySearch([4,5,6,7,0,1,2], 0))  # Expected: 4
+    print(s.binarySearch([4,5,6,7,0,1,2], 3))  # Expected: None
